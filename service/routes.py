f4322e42f35faccf3dca4e3503e89a7247694ff7
sokoeurn88 <92101403+sokoeurn88@users.noreply.github.com>
2023-04-08 18:58:42 -0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -57,7 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
-def test_get_account_not_found(self):
+#def test_get_account_not_found(self):
     """It should not Read an Account that is not found"""
     resp = self.client.get(f"{BASE_URL}/0")
     self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
